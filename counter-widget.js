395ccdc39b65fdc328c362dc54a24a3e03357b84
kevindufraisse <kevindufraisse@hotmail.fr>
2023-05-02 10:35:58 +0100

    Update counter-widget.js

--- counter-widget.js
+++ counter-widget.js
@@ -1,26 +1,28 @@
-(function() {
+(function () {
   function updateCounter() {
     var min = 130;
     var max = 333;
     var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    var counterElement = document.querySelector('.counter-widget-popup .random-number');
+    var counterElement = document.querySelector(".counter-widget-popup .random-number");
     if (counterElement) {
       counterElement.textContent = randomNumber;
     }
   }
 
   function initCounterWidget() {
-    var container = document.querySelector('.counter-widget-popup');
+    var container = document.querySelector(".counter-widget-popup");
     if (container) {
       container.innerHTML = '<span>Personne en ligne: </span><span class="random-number">100</span>';
-      setInterval(function() {
+      function updateWithRandomInterval() {
         var minInterval = 20;
         var maxInterval = 40;
         var randomInterval = Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval;
         updateCounter();
-      }, randomInterval * 1000);
+        setTimeout(updateWithRandomInterval, randomInterval * 1000);
+      }
+      updateWithRandomInterval();
     }
   }
 
-  document.addEventListener('DOMContentLoaded', initCounterWidget);
+  document.addEventListener("DOMContentLoaded", initCounterWidget);
 })();
