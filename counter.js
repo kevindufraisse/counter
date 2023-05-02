38a3a8fd9c492e95330c6a5ccd7627d268126c2e
kevindufraisse <kevindufraisse@hotmail.fr>
2023-05-02 10:42:19 +0100

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,29 @@
+
+(function () {
+    function updateCounter() {
+      var min = 130;
+      var max = 333;
+      var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      var counterElement = document.querySelector(".counter-widget-popup .random-number");
+      if (counterElement) {
+        counterElement.textContent = randomNumber;
+      }
+    }
+  
+    function initCounterWidget() {
+      var container = document.querySelector(".counter-widget-popup");
+      if (container) {
+        container.innerHTML = '<span>Personne en ligne: </span><span class="random-number">100</span>';
+        function updateWithRandomInterval() {
+          var minInterval = 20;
+          var maxInterval = 40;
+          var randomInterval = Math.floor(Math.random() * (maxInterval - minInterval + 1)) + minInterval;
+          updateCounter();
+          setTimeout(updateWithRandomInterval, randomInterval * 1000);
+        }
+        updateWithRandomInterval();
+      }
+    }
+  
+    document.addEventListener("DOMContentLoaded", initCounterWidget);
+  })()
